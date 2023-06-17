40e3b01a12d9b23225f173a79afc2bdd37d8d87a
Mnishthakur <102964888+Mnishthakur@users.noreply.github.com>
2023-06-17 13:58:35 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 1, 2, 3, 4, 2, 3, 5, 6, 4, 7, 8, 1, 9, 1 };
+        List<int> uniqueElements = GetUniqueElements(arr);
+
+        Console.WriteLine("Unique Elements:");
+        foreach (int element in uniqueElements)
+        {
+            Console.WriteLine(element);
+        }
+    }
+
+    static List<int> GetUniqueElements(int[] arr)
+    {
+        List<int> uniqueElements = new List<int>();
+        Dictionary<int, bool> duplicates = new Dictionary<int, bool>();
+
+        foreach (int num in arr)
+        {
+            if (!duplicates.ContainsKey(num))
+            {
+                duplicates[num] = true;
+                uniqueElements.Add(num);
+            }
+        }
+
+        return uniqueElements;
+    }
+}
