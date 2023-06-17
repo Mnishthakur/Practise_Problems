a5ad508625e10b16bd1f847f822a0022c64abc31
Mnishthakur <102964888+Mnishthakur@users.noreply.github.com>
2023-06-17 14:28:24 +0530

    [Manish Thakur]: Added 5th program

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int rows = 8;
+
+        for (int i = rows; i >= 1; i--)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
